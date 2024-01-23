b4250aeb139c704a33ea15b951917b98dbeb1725
Luong-Dev <ducluongwebdev@gmail.com>
2024-01-23 18:58:35 +0700

    add discoun code

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TypeDiscountCodeController;
+use App\Http\Controllers\DiscountCodeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+//Route::group(['prefix' => 'parent-location'], function () {
+//    Route::get('', [ParentLocationController::class, 'index']);
+//    Route::post('store', [ParentLocationController::class, 'store']);
+//    Route::get('edit/{id}', [ParentLocationController::class, 'show']);
+//    Route::put('update/{id}', [ParentLocationController::class, 'update']);
+//    Route::delete('delete/{id}', [ParentLocationController::class, 'destroy']);
+//});
+
+Route::group(['prefix' => 'type-discount-code'], function () {
+    Route::get('', [TypeDiscountCodeController::class, 'index']);
+    Route::post('store', [TypeDiscountCodeController::class, 'store']);
+    Route::get('edit/{id}', [TypeDiscountCodeController::class, 'show']);
+    Route::put('update/{id}', [TypeDiscountCodeController::class, 'update']);
+    Route::delete('delete/{id}', [TypeDiscountCodeController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'discount-code'], function () {
+    Route::get('', [DiscountCodeController::class, 'index']);
+    Route::post('store', [DiscountCodeController::class, 'store']);
+    Route::get('edit/{id}', [DiscountCodeController::class, 'show']);
+    Route::put('update/{id}', [DiscountCodeController::class, 'update']);
+    Route::delete('delete/{id}', [DiscountCodeController::class, 'destroy']);
 });
