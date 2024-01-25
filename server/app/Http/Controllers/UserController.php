799662ef843953d04db10d7742e6e1afb369b827
thuannm Bsscommerce <thuannmph19038@nnmph19038@fpt.edu.vn>
2024-01-25 23:09:07 +0700

    thuan BA-user edit user controller

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -10,85 +10,126 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::with('typeUser')->get();
+        try {
+            $users = User::with('typeUser')->get();
 
-        return response()->json($users);
+            return response()->json(['message' => 'Lấy dữ liệu thành công', 'uesrs' => $users]);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Có lỗi trong quá trình lấy dữ liệu','error' => $e->getMessage()]);
+        }
+        
     }
 
     public function show($userId)
     {
-        $user = User::with('typeUser')->find($userId);
+        try {
+            $user = User::with('typeUser')->find($userId);
 
-        if (!$user) {
-            return response()->json(['message' => 'User not found'], 404);
+            if (!$user) {
+                return response()->json(['message' => 'Người dùng không tồn tại'], 404);
+            }
+
+            return response()->json(['message' => 'Lấy dữ liệu thành công', 'uesr' => $user]);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Có lỗi trong quá trình lấy dữ liệu','error' => $e->getMessage()]);
         }
-
-        return response()->json($user);
+        
     }
 
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'user_type_id' => 'required|exists:user_types,id',
-            'email' => 'required|email|unique:users',
-            'name' => 'required',
-            'password' => 'required',
-            'phone_number' => 'nullable',
-            'address' => 'nullable',
-            'description' => 'nullable',
-            'location' => 'nullable',
-        ], $this->getValidationMessages());
-
-        $user = User::create($validatedData);
-
-        return response()->json($user, 201);
+        try {
+            $request->validate([
+                'user_type_id' => 'required|exists:type_users,id|numeric',
+                'email' => 'required|email|unique:users',
+                'name' => 'required',
+                'password' => 'required',
+                'phone_number' => 'nullable',
+                'address' => 'nullable',
+                'description' => 'nullable',
+                'location' => 'nullable',
+            ], $this->getValidationMessages());
+    
+            $user = new User();
+            $user->user_type_id = $request->input('user_type_id');
+            $user->email = $request->input('email');
+            $user->name = $request->input('name');
+            $user->password = $request->input('password');
+            $user->phone_number = $request->input('phone_number');
+            $user->address = $request->input('address');
+            $user->description = $request->input('description');
+            $user->location = $request->input('location');
+            $user->save();
+    
+            return response()->json(['message' => 'Thêm mới người dùng thành công', 'uesr' => $user]);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Có lỗi trong quá trình  thêm người dùng','error' => $e->getMessage()]);
+        }
     }
 
     public function update(Request $request, $userId)
     {
-        $validatedData = $request->validate([
-            'user_type_id' => 'required|exists:user_types,id',
-            'email' => [
-                'required',
-                'email',
-                Rule::unique('users')->ignore($userId),
-            ],
-            'name' => 'required',
-            'password' => 'required',
-            'phone_number' => 'nullable',
-            'address' => 'nullable',
-            'description' => 'nullable',
-            'location' => 'nullable',
-        ], $this->getValidationMessages());
-
-        $user = User::find($userId);
-
-        if (!$user) {
-            return response()->json(['message' => 'User not found'], 404);
+        try {
+            $request->validate([
+                'user_type_id' => 'required|exists:type_users,id|numeric',
+                'email' => [
+                    'required',
+                    'email',
+                    Rule::unique('users')->ignore($userId),
+                ],
+                'name' => 'required',
+                'password' => 'required',
+                'phone_number' => 'nullable',
+                'address' => 'nullable',
+                'description' => 'nullable',
+                'location' => 'nullable',
+            ], $this->getValidationMessages());
+    
+            $user = User::find($userId);
+    
+            if (!$user) {
+                return response()->json(['message' => 'User not found'], 404);
+            }
+    
+            $user->user_type_id = $request->input('user_type_id');
+            $user->email = $request->input('email');
+            $user->name = $request->input('name');
+            $user->password = $request->input('password');
+            $user->phone_number = $request->input('phone_number');
+            $user->address = $request->input('address');
+            $user->description = $request->input('description');
+            $user->location = $request->input('location');
+            $user->save();
+    
+            return response()->json(['message' => 'Cập nhật người dùng thành công', 'uesr' => $user]);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Có lỗi trong quá trình  cập nhật người dùng','error' => $e->getMessage()]);
         }
-
-        $user->update($validatedData);
-
-        return response()->json($user);
+        
     }
 
     public function destroy($userId)
     {
-        $user = User::find($userId);
+        try {
+            $user = User::find($userId);
 
-        if (!$user) {
-            return response()->json(['message' => 'User not found'], 404);
+            if (!$user) {
+                return response()->json(['message' => 'Người dùng không tồn tại'], 404);
+            }
+
+            $user->delete();
+
+            return response()->json(['message' => 'Xóa người dùng thành công']);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Xóa người dùng thất bại','error' => $e->getMessage()]);
         }
-
-        $user->delete();
-
-        return response()->json(['message' => 'User deleted']);
     }
 
     private function getValidationMessages()
     {
         return [
             'user_type_id.required' => 'The user type is required.',
+            'user_type_id.numeric' => 'id type user bắt buộc là số',
             'user_type_id.exists' => 'The selected user type is invalid.',
             'email.required' => 'The email is required.',
             'email.email' => 'The email must be a valid email address.',
