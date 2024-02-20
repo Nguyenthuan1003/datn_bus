d929eac4233a9fd60ee18fd169d03e9c039b8764
thuannm Bsscommerce <thuannmph19038@nnmph19038@fpt.edu.vn>
2024-02-20 23:25:36 +0700

    thuannm BA-bill create controller and route api bill

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BillController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::group(['prefix' => 'bill'], function () {
+    Route::get('', [BillController::class, 'index']);
+    Route::post('store', [BillController::class, 'store']);
+    Route::get('edit/{id}', [BillController::class, 'show']);
+    Route::put('update/{id}', [BillController::class, 'update']);
+    Route::delete('delete/{id}', [BillController::class, 'destroy']);
 });
