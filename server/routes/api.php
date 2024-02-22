6d513ebe27b30f0634127245ae05ca0eaefed0b5
thuannm Bsscommerce <thuannmph19038@nnmph19038@fpt.edu.vn>
2024-02-22 23:44:06 +0700

    create controller and route api routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RouteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'route'], function () {
+    Route::get('', [RouteController::class, 'index']);
+    Route::post('store', [RouteController::class, 'store']);
+    Route::get('edit/{id}', [RouteController::class, 'show']);
+    Route::put('update/{id}', [RouteController::class, 'update']);
+    Route::delete('delete/{id}', [RouteController::class, 'destroy']);
 });
