8c53b9b2ef3048a0a69bacbd6caa36ab7a657a6a
Luong-Dev <ducluongwebdev@gmail.com>
2024-03-04 00:30:37 +0700

    BA-statistical - statistical for homepage, general, revenue, route

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StatisticalController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::group(['prefix' => 'statistical'], function () {
+    Route::get('home', [StatisticalController::class, 'home']);
+    Route::get('general', [StatisticalController::class, 'general']);
+    Route::get('revenue-trip-year', [StatisticalController::class, 'revenueTripYear']);
+    Route::get('revenue-trip-about', [StatisticalController::class, 'revenueTripAbout']);
+    Route::get('user', [StatisticalController::class, 'user']);
+    Route::get('route', [StatisticalController::class, 'route']);
+    Route::get('route-for-year', [StatisticalController::class, 'routeForYear']);
 });
