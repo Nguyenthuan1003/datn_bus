28e429fda1e69c3eaed73a1d90279b9a8c4c73fc
Luong-Dev <ducluongwebdev@gmail.com>
2024-03-04 22:35:18 +0700

    hot-fix-conflict-develop-04-04-2024 - remove code conflict

--- server/app/Models/Location.php
+++ server/app/Models/Location.php
@@ -9,18 +9,14 @@ class Location extends Model
 
     protected $table = 'locations';
 
-<<<<<<< HEAD
     protected $fillable = [
         'name',
         'description',
         'image',
         'parent_location_id'
     ];
-    public function parentLocation()
-    {
-=======
+
     public function parentLocation() {
->>>>>>> ed3f9f48b0c6e8ad586393cfdc2d53e3095f6581
         return $this->belongsTo(ParentLocation::class, 'parent_location_id');
     }
 }
