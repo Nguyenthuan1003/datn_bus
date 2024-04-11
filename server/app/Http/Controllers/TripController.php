fb4f05e0e0583c324111145a77b97f238e8181dc
Luong-Dev <ducluongwebdev@gmail.com>
2024-04-11 23:09:00 +0700

    BA-trip update api update for trip

--- server/app/Http/Controllers/TripController.php
+++ server/app/Http/Controllers/TripController.php
@@ -390,7 +390,7 @@ class TripController extends Controller
             return response()->json([
                 'message' => 'Đã xảy ra lỗi khi xử lý dữ liệu',
                 "status" => "fail",
-                'error' => $e->getMessage()
+//                'error' => $e->getMessage()
             ]);
         }
     }
