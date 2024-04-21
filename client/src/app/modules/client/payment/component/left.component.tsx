7d9391ae857fb289a3b53d5c4412f7f93607d554
Nguyễn Văn Tuấn <49591858+maohiemgia@users.noreply.github.com>
2024-04-21 08:28:33 +0700

    Fe/fix client payment (#73)
    * FE/linh-update-payment
    
    * update
    
    ---------
    
    Co-authored-by: quanglinh <linhdqph27370@fpt.edu.vn>

--- client/src/app/modules/client/payment/component/left.component.tsx
+++ client/src/app/modules/client/payment/component/left.component.tsx
@@ -1,9 +1,6 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { useCartRedux } from '../../redux/hook/useCartReducer'
-import { addBill, cancelBill, paymentVNP } from '~/app/api/bill/bill.api'
-import { Link } from 'react-router-dom'
-import { getOneUser } from '~/app/api/auth/auth.api'
-import { updateBillAndSendMail } from '~/app/api/bill/bill.api'
+import { cancelBill, paymentVNP } from '~/app/api/bill/bill.api'
 import { Spin, message } from 'antd'
 import { LoadingOutlined } from '@ant-design/icons';
 const LeftComponent = () => {
@@ -11,26 +8,21 @@ const LeftComponent = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [selectedPayment, setSelectedPayment] = useState<string | null>(null);
     const { data: { cart } } = useCartRedux()
-   
-    
-    const paymentRef = useRef<any>(null);
-    console.log('paymentRef',paymentRef);
+    // console.log('paymentRef',paymentRef);
     useEffect(() => {
         window.scrollTo(0, 0); // Cuộn lên đầu trang khi component được load lại
     }, []); // useEf
-    console.log('cart', cart);
     const dataBill: any = localStorage.getItem('bill_user')
     const ObDataBill = JSON.parse(dataBill)
 
     const handelDataPaymetSanbox = () => {
-        paymentVNP({ amount: cart?.total_money_after_discoun, code_bill: cart?.code_bill })
+        // paymentVNP({ amount: cart?.total_money_after_discoun, code_bill: cart?.code_bill })
+        paymentVNP({ amount: ObDataBill?.total_money_after_discount, code_bill: ObDataBill?.code_bill })
             .then((res: any) => {
                 // Chuyển hướng đến trang thanh toán của bên thứ ba
                 window.location.href = res?.data?.url;
-            })
-       
-        
-            .catch((error: any) => {
+            }) 
+           .catch((error: any) => {
                 // Xử lý lỗi nếu có
                 console.error("Error:", error);
             });
@@ -42,6 +34,8 @@ const LeftComponent = () => {
             const confirmed = window.confirm('Bạn có muốn hủy đơn hàng không ?');
             if (confirmed) {
                 const { data }: any = await cancelBill(idBill);
+                    localStorage.removeItem("bill_user")
+                    localStorage.removeItem("cart")
 
                 fetch('http://127.0.0.1:8000/api/rt/seat')
                 .then(
@@ -55,13 +49,15 @@ const LeftComponent = () => {
                         console.log("data",data);
                     })
                     }
+                    
                 )
+                
                 .catch(err => {
                     console.log('Error :-S', err)
                 });
 
                 if (data) {
-                    message.success(data.message);
+                    message.success("Hủy đơn hàng thành công!");
                     // Chờ 10 giây trước khi chuyển hướng sang trang chủ
                     setTimeout(() => {
                         window.location.href = '/'; // Điều hướng đến trang chủ
@@ -95,55 +91,6 @@ const LeftComponent = () => {
             handelDataPaymetSanbox(); // Thay thế bằng hàm xử lý thanh toán thực tế
         }
     };
-
-    //  const [idUser,setIdUser]=useState<any>({})
-    //  const accsetoken:any=localStorage.getItem("token")
-    //  useEffect(()=>{
-    //     getOneUser(accsetoken).then((res:any)=>{
-    //         setIdUser(res?.data?.user)
-    //     })
-    //  },[])
-    //  console.log('idUser',idUser)
-    // const handelDataPaymetSanbox = async () =>{
-    //     try {
-    //         const billData = {
-    //             full_name: cart?.full_name,
-    //             phone_number: cart?.phone_number,
-    //             email: cart?.email,
-    //             total_money: cart?.total_money,
-    //             total_money_after_discount: cart?.total_money_after_discoun,
-    //             seat_id: JSON.stringify(cart?.seat_id),
-    //             trip_id: cart?.trip_id,
-    //             status_pay: "0",
-    //             type_pay: "0",
-    //             total_seat: cart?.seat_id?.length ,
-    //             code_bill: cart?.code_bill,
-    //             discount_code_id: null,
-    //             user_id:idUser?.id
-    //         };
-    //         // Gọi API để lưu hóa đơn
-    //         console.log('billData',billData);
-
-    //         await addBill(billData);            
-
-    //         // Sau khi hóa đơn được lưu thành công, thực hiện thanh toán
-    //         const res = await paymentVNP({ amount: cart?.total_money_after_discoun , code_bill:cart?.code_bill});
-
-    //         // Sau khi nhận được URL thanh toán, chuyển hướng tới trang thanh toán
-    //         window.location.href = res?.data?.url;
-    //     } catch (error:any) {
-    //         if (error.response) {
-    //             // Lỗi từ API
-    //             console.error('API error:', error.response.data);
-    //         } else if (error.request) {
-    //             // Lỗi từ không có phản hồi từ server
-    //             console.error('No response received:', error.request);
-    //         } else {
-    //             // Lỗi khác
-    //             console.error('Error:', error.message);
-    //         }
-    //     }
-    // }
     return (
         <div>
             {isLoading && (
